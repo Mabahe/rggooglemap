216783d3ea08d193ca864a151612e2454a707f71
just2b <just2b@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-02 07:41:10 +0000

    - removed some files like googleShort.js, gxmarker.js (in root dir) - cgl cleanup of tca.php & ext_tables.php - removed not needed fields in tt_address like tab, cat1 - removed not needed fields in category like color
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/rggooglemap/trunk@19812 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,11 +4,7 @@
 CREATE TABLE tt_address (
     tx_rggooglemap_lng tinytext NOT NULL,
     tx_rggooglemap_lat tinytext NOT NULL,
-    tx_rggooglemap_display tinyint(3) DEFAULT '0' NOT NULL,
     tx_rggooglemap_cat int(11) DEFAULT '0' NOT NULL,
-    tx_rggooglemap_tab int(11) DEFAULT '0' NOT NULL,
-    tx_rggooglemap_cat2 tinytext NOT NULL,
-    tx_rggooglemap_ce blob NOT NULL,
 );
 
 
@@ -28,7 +24,6 @@ CREATE TABLE tx_rggooglemap_cat (
 	hidden tinyint(4) DEFAULT '0' NOT NULL,
   title tinytext NOT NULL,
   descr text NOT NULL,
-  color tinytext NOT NULL,
   image blob NOT NULL,
   tabprefix tinytext NOT NULL,
 	parent_uid int(11) unsigned DEFAULT '0' NOT NULL,
@@ -36,21 +31,3 @@ CREATE TABLE tx_rggooglemap_cat (
   PRIMARY KEY (uid),
   KEY parent (pid)
 );
-
-#
-# Table structure for table 'tx_rggooglemap_generic'
-#
-#CREATE TABLE tx_rggooglemap_generic (
-#    uid int(11) NOT NULL auto_increment,
-#    pid int(11) DEFAULT '0' NOT NULL,
-#    tstamp int(11) DEFAULT '0' NOT NULL,
-#    crdate int(11) DEFAULT '0' NOT NULL,
-#    cruser_id int(11) DEFAULT '0' NOT NULL,
-#    deleted tinyint(4) DEFAULT '0' NOT NULL,
-#    hidden tinyint(4) DEFAULT '0' NOT NULL,
-#    tablename tinytext NOT NULL,
-#    mapping text NOT NULL,
-#    
-#    PRIMARY KEY (uid),
-#    KEY parent (pid)
-#);
